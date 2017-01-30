d59e32cd68b137f95b520ff59b2b40e0609cf1f5
TeeJay <xebitstudios@yahoo.com>
2017-01-30 11:21:46 -0500

    added a comment

--- test/summer-test.js
+++ test/summer-test.js
@@ -1,8 +1,9 @@
-var test = require('tape')
-var summer = require('../')
+var test = require('tape');
+var summer = require('../');
 
 test('summer', function (t) {
-  var winter = summer(-40)
-  t.equal(winter(40), 0, 'should be equal')
-  t.end()
+  var winter = summer(-40);
+  console.log('ha ha ha ha');
+  t.equal(winter(40), 0, 'should be equal');
+  t.end();
 })
